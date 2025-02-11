a7f990ebdf552fd9d3d5801a0ca17bdf452c680d
MaxOhNoo <87615947+MaxOhNoo@users.noreply.github.com>
2025-02-10 22:27:01 -0800

    Initial commit: hero image, basic description, 3 images, what to expect

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import './style.css';
+</script>
+
+<img src="/poster.jpg" alt="Multicultural Night" class="hero" />
+
+<p>
+    Join us for a celebration of Livermore’s diverse cultures, traditions, and backgrounds!
+    Experience an evening filled with performances, food, music, dance, art, and interactive
+    activities that showcase the many cultures in our community.
+</p>
+
+<img src="panorama.png" alt="Many colorful dancers and a few mariachi" class="panorama" />
+
+<div class="container">
+    <div class="column">
+        <h1>What to Expect:</h1>
+        <p class="text-box">
+            ✅ Live cultural performances
+            <br />✅ Traditional and international cuisine
+            <br />✅ Art displays & interactive activities
+            <br />✅ Opportunities to connect and learn
+        </p>
+    </div>
+
+    <img src="/tables.png" alt="Crowded tables during the event" class="tables" />
+</div>
